16fadef682e480b21a9301a92200549453aa8b42
Yu-Fan Yang <yyang334@myseneca.ca>
2025-01-18 21:19:22 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Yu-Fan Yang
+#Author ID: 154742225
+#Date Created: 2025/01/18
+
+Timer = 10
+while Timer > 0:
+    print(Timer)
+    Timer = Timer - 1
+
+print('blast off!')
+
