157050daa5018202ef0064055b0cf5be373cf45f
Yu-Fan Yang <yyang334@myseneca.ca>
2025-01-18 21:19:34 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Yu-Fan Yang
+#Author ID: 154742225
+#Date Created: 2025/01/18
+
+import sys
+Timer = int(sys.argv[1])
+while Timer > 0:
+    print(Timer)
+    Timer = Timer - 1
+
+print('blast off!')
+
